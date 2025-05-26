2c4878c4b6cdabe37d1dca8e1847679079826f0d
Finn <finn.dijkstra@gmail.com>
2025-05-26 11:15:50 +0200

    All kinds of stuff for spheres (complex and real). Integrate stuff, random points, probability distribution basics, complex "double cap"

---
+++ SphereBasics.py
@@ -0,0 +1,413 @@
+import scipy as sp
+import scipy.stats as st
+from scipy.interpolate import splrep, PPoly, UnivariateSpline
+import math
+import cmath
+import numpy as np
+# from scipy.interpolate import UnivariateSpline
+from line_profiler import LineProfiler
+
+
+polar2z = lambda r, theta: r * np.exp(1j * theta)
+z2polar = lambda z: (np.abs(z), np.angle(z))
+
+# Makes an autocorrelation function of double caps with radius c
+def cap_fraction_grid(t_array, n, u1_points=200, u2_points=200, c=1.0 / np.sqrt(2.0)):
+    t_array = np.atleast_1d(t_array)
+
+    # Create uniform grids
+    # make a grid for the poles (cos(thet), sin(thet), 0, 0,...)
+    perspectivePoleInnerGrid = np.linspace(-1, 1, u1_points)
+    sinPPIG = np.sin(np.arccos(perspectivePoleInnerGrid))
+    # the weight of the group with (cos(thet),.....)
+    pPIGWeight = np.abs(np.sin(np.arccos(perspectivePoleInnerGrid))) ** (n - 3)
+
+    # Make a grid for the cos of the remainder of w with innerproduct t to v wrt (0,1,0,0,0,...) and its weight
+    doubleProjectiveGridTheta = np.linspace(0, 2 * math.pi, u2_points)
+    cosDPGT = np.cos(doubleProjectiveGridTheta)
+    sinDPGT = np.abs(np.sin(doubleProjectiveGridTheta))
+    dPGTWeight = sinDPGT ** (n - 3)
+    dPGTTotWeight = np.sum(dPGTWeight)
+
+    # Create mask if cap wrt pole is on v
+    maskCap = (np.abs(perspectivePoleInnerGrid) > c)
+    pPIGTotWeight = np.sum(pPIGWeight[maskCap]) / 2
+    # Create mask if cap wrt pole has atleast 1 point that has innerproduct t
+    term1 = np.abs(t_array[:, None] * perspectivePoleInnerGrid[None, :])
+    term2 = np.sqrt(1 - t_array[:, None] ** 2) * np.sqrt(1 - perspectivePoleInnerGrid[None, :] ** 2)
+    maskTCap = ((term1 + term2) > c)
+    maskTot = maskTCap * maskCap
+
+    # For each t, for each cap, test if each cosine of a point (with innerproduct t) wrt e_2 is in the cap and add its
+    # weight if that is the case
+    outputTVol = np.zeros_like(t_array)
+    for tIDX in range(t_array.shape[0]):
+        t = t_array[tIDX]
+        tOrth = math.sqrt(1 - t ** 2)
+        curTDensity = np.zeros(u1_points)
+        for pole in range(u1_points):
+            if maskCap[pole] & maskTCap[tIDX, pole]:
+                poleInner = perspectivePoleInnerGrid[pole]
+                poleOrth = sinPPIG[pole]
+                innerFactor = t * poleInner
+                OrthFactor = tOrth * poleOrth
+                OrthComp = OrthFactor * cosDPGT
+                innerPT = OrthComp + innerFactor
+                capTAngMask = (innerPT > c)
+                curTDensity[pole] = np.sum(dPGTWeight[capTAngMask]) / dPGTTotWeight
+        outputTVol[tIDX] = np.inner(curTDensity, pPIGWeight) / pPIGTotWeight
+    return outputTVol
+
+
+def complexInnerproduct(radiiMatrix, anglesMatrix):
+    nrOfColumns = radiiMatrix.shape[0]
+    realPartMatrix = np.zeros((nrOfColumns, nrOfColumns))
+    imagPartMatrix = np.zeros((nrOfColumns, nrOfColumns))
+    for vec1Idx in range(nrOfColumns):
+        vec1radii = radiiMatrix[vec1Idx]
+        vec1angles = anglesMatrix[vec1Idx]
+        for vec2Idx in range(nrOfColumns):
+            vec2radii = radiiMatrix[vec2Idx]
+            vec2angles = anglesMatrix[vec2Idx]
+            newAngles = vec1angles - vec2angles
+            newRadii = vec1radii * vec2radii
+            realPart = np.dot(newRadii, np.cos(newAngles))
+            imagPart = np.dot(newRadii, np.sin(newAngles))
+            realPartMatrix[vec1Idx][vec2Idx] = realPart
+            imagPartMatrix[vec1Idx][vec2Idx] = imagPart
+    return realPartMatrix, imagPartMatrix
+
+
+def decompose(realParts, imaginaryParts):
+    columnNr = realParts.shape[0]
+    radiiMatrix = np.power(np.power(realParts, 2) + np.power(imaginaryParts, 2), 1 / 2)
+    nonZeroMask = (radiiMatrix > 0)
+    angleMatrix = np.zeros((columnNr, columnNr))
+    angleMatrix[nonZeroMask] = np.arccos(realParts[nonZeroMask] / radiiMatrix[nonZeroMask])
+    overPiMask = (imaginaryParts < 0)
+    angleMatrix[overPiMask] = - angleMatrix[overPiMask]
+    # for vec1Idx in range(columnNr):
+    #     for vec2Idx in range(columnNr):
+    #         realPart = realParts[vec1Idx][vec2Idx]
+    #         imagPart = imaginaryParts[vec1Idx][vec2Idx]
+    #         radiiMatrix[vec1Idx][vec2Idx] = realPart**2 + imagPart**2
+    #         if realPart == 0
+    return radiiMatrix, angleMatrix
+
+
+# Takes dimension and desired size and gives the weight density for the real Sphere
+def realSphereDensity(sphereDim, arraySize):
+    innerproductArray = np.linspace(-1, 1, arraySize)
+    constFactor = sp.special.gamma(sphereDim / 2) / (sp.special.gamma(sphereDim / 2) * math.sqrt(math.pi) * arraySize)
+    return constFactor * np.power((1 - np.power(innerproductArray, 2)), (sphereDim - 3) / 2)
+
+
+# Takes dimension and desired size and gives the weight density of radii for the complex Sphere
+def complexSphereDensity(sphereDim, arraySize):
+    innerproductArray = np.linspace(0, 1, arraySize)
+    constFactor = 2 * (sphereDim - 1) / arraySize
+    return constFactor * np.power((1 - np.power(innerproductArray, 2)), (sphereDim - 2)) * innerproductArray
+
+
+class real_sphere_pdf(st.rv_continuous):
+    shapes = "c"
+
+    def _argcheck(self, c):
+        # only allow positive c, for instance
+        return c > 0
+
+    def _pdf(self, x, c):
+        constFactor = sp.special.gamma(c / 2) / (
+                sp.special.gamma(c / 2) * math.sqrt(math.pi))
+        return constFactor * (1 - x ** 2) ** ((c - 3) / 2)
+
+
+class complex_sphere_pdf(st.rv_continuous):
+    shapes = "c"
+
+    def _argcheck(self, c):
+        # only allow positive c, for instance
+        return c > 0
+
+    def _pdf(self, x, c):
+        return 2 * (c - 1) * x * (1 - x ** 2) ** (c - 2)
+
+
+class piecewisePolyPdf(st.rv_continuous):
+    shapes = "poly"
+
+    def _argcheck(self, poly):
+        # only allow positive c, for instance
+        return isinstance(poly, PPoly)
+
+    def _pdf(self, x, poly):
+        return poly(x)          # __call__
+
+
+
+def createRandomPointsReal(baseDim, nrPoints, checkForMistakesBool=False):
+    nrPoints = max(2, nrPoints)
+    randomPointMatrix = np.zeros((nrPoints, baseDim))
+    randomPointMatrix[0][0] = 1
+    randomPointMatrix[1][1] = 1
+    rsDistr = real_sphere_pdf(a=-1, b=1,
+                              name=f'Equal distrubution on real sphere in R^{baseDim}')
+    pdfList = [rsDistr(c=baseDim - layerIdx)
+               for layerIdx in range(baseDim - 2)]
+    variatesArray = np.array([pdfList[idx].rvs(size=nrPoints) for idx in range(baseDim - 2)])
+    angles = st.uniform.rvs(scale=math.pi, size=nrPoints)
+    signs = np.random.choice([-1, 1], size=nrPoints)
+    for pointIdx in range(2, nrPoints):
+        rSquared = 1
+        degOfVecFreedom = min(pointIdx, baseDim - 1)
+        pointVec = np.zeros(baseDim)
+        for coordIdx in range(degOfVecFreedom):
+            r = math.sqrt(rSquared)
+            # catch two dimensional case (due to infinite densities as |r|=1)
+            if coordIdx == baseDim - 2:
+                angle = angles[pointIdx]
+                pointVec[baseDim - 2] = r * math.cos(angle)
+                rSquared -= (r * math.cos(angle)) ** 2
+            # find v[coordIdx]=<e_{coordIdx},v> with prior knowledge of v[i<coordIdx] results in n-coordIdx sphere
+            # with radius r = sqrt(1-v_0^2 - v_1^2 -...- v_{coordIdx-1}^2)
+            else:
+                coordValue = r * variatesArray[coordIdx][pointIdx]
+                rSquared -= coordValue ** 2
+                pointVec[coordIdx] = coordValue
+            # Should not happen tbh, only happens if calcs are wrong or if a variate is exactly 1)
+            if rSquared <= 0:
+                print("Situation with prob<=0")
+                break
+        pointVec[degOfVecFreedom] = math.sqrt(rSquared) * signs[pointIdx]
+        randomPointMatrix[pointIdx] = pointVec
+    if checkForMistakesBool:
+        squaredCoordinates = np.power(randomPointMatrix, 2)
+        vectorSizes = np.sum(squaredCoordinates, axis=1)
+        if np.any((vectorSizes - 1) > 0.00001):
+            print("mistake found")
+    return randomPointMatrix
+
+
+def createRandomPointsComplex(baseDim, nrPoints, baseZ=0+0j, checkForMistakesBool=False):
+    nrPoints = max(2, nrPoints)
+    baseRad, baseAngle = z2polar(baseZ)
+    randomPointMatrixRadii = np.zeros((nrPoints, baseDim))
+    randomPointMatrixRadii[0][0] = 1
+    randomPointMatrixRadii[1][0] = baseRad
+    randomPointMatrixRadii[1][1] = 1-baseRad
+    csDistr = complex_sphere_pdf(a=0, b=1,
+                                 name=f'Equal distrubution on complex sphere in R^{baseDim}')
+    pdfList = [csDistr(c=baseDim - layerIdx)
+               for layerIdx in range(baseDim - 1)]
+    variatesArray = np.array([pdfList[idx].rvs(size=nrPoints) for idx in range(baseDim - 1)])
+    coordinateAngles = st.uniform.rvs(scale=math.pi, size=(nrPoints, baseDim))
+    coordinateAngles[1][0] = baseAngle
+    unangledCoordinates = np.zeros((nrPoints, baseDim), dtype=bool)
+    smallestCoordinate = min(baseDim, nrPoints)
+    for i in range(smallestCoordinate):
+        for j in range(i, smallestCoordinate):
+            unangledCoordinates[i][j] = True
+    coordinateAngles[unangledCoordinates] = 0
+    for pointIdx in range(2, nrPoints):
+        rSquared = 1
+        degOfVecFreedom = min(pointIdx, baseDim - 1)
+        pointVec = np.zeros(baseDim)
+        for coordIdx in range(degOfVecFreedom):
+            r = math.sqrt(rSquared)
+            # find v[coordIdx]=<e_{coordIdx},v> with prior knowledge of v[i<coordIdx] results in n-coordIdx sphere
+            # with radius r = sqrt(1-v_0^2 - v_1^2 -...- v_{coordIdx-1}^2)
+            coordValue = r * variatesArray[coordIdx][pointIdx]
+            rSquared -= coordValue ** 2
+            pointVec[coordIdx] = coordValue
+            # Should not happen tbh, only happens if calcs are wrong or if a variate is exactly 1)
+            if rSquared <= 0:
+                print("Situation with prob<=0")
+                break
+        pointVec[degOfVecFreedom] = math.sqrt(rSquared)
+        randomPointMatrixRadii[pointIdx] = pointVec
+    if checkForMistakesBool:
+        squaredCoordinates = np.power(randomPointMatrixRadii, 2)
+        vectorSizes = np.sum(squaredCoordinates, axis=1)
+        if np.any(vectorSizes != 1):
+            print("mistake found")
+    complexMatrix = polar2z(randomPointMatrixRadii, coordinateAngles)
+    return complexMatrix, randomPointMatrixRadii, coordinateAngles
+
+
+def complexTester():
+    randomComplexSpherePointRadii, randomAngles = createRandomPointsComplex(4, 6, checkForMistakesBool=True)[1:]
+    realPartInnerProducts, imaginaryPartInnerProducts = complexInnerproduct(randomComplexSpherePointRadii, randomAngles)
+    radii, angles = decompose(realPartInnerProducts, imaginaryPartInnerProducts)
+    radii2, angles2 = z2polar(realPartInnerProducts + 1j * imaginaryPartInnerProducts)
+
+    complexPointNotation = polar2z(randomComplexSpherePointRadii, randomAngles)
+    complexInnerProductVals = (complexPointNotation @ np.conjugate(complexPointNotation).T)
+    radii3, angles3 = z2polar(complexInnerProductVals)
+    return radii, angles, radii2, angles2, radii3, angles3
+
+
+def complexAngleIntegrator(inputDatapoints, angleAxis=1, normalize=False):
+    integratedPoints = np.sum(inputDatapoints, axis=angleAxis)/inputDatapoints.shape[angleAxis]
+    if normalize:
+        integratedPoints = integratedPoints / np.sum(integratedPoints)
+    return integratedPoints
+
+
+def complexWeightApply(inputDatapoints, complexDimension, radiusArray=None, radiusAxis=0):
+    inputDataSize = inputDatapoints.shape[radiusAxis]
+    if radiusArray is None:
+        radiusArray = np.linspace(0,1, inputDataSize)
+    weightArray = 2*(complexDimension - 1)*radiusArray*np.power((1-np.square(radiusArray)), complexDimension-2)/inputDataSize
+    shape = [1] * inputDatapoints.ndim
+    shape[radiusAxis] = weightArray.shape[0]
+    weightNDimArray = weightArray.reshape(shape)
+    return inputDatapoints*weightNDimArray
+
+
+def completeIntegrator(inputDatapoints, complexDimension, radiusArray=None, applyWeightBool=True,
+                       radiusAxis=0, angleAxis=1):
+    if applyWeightBool:
+        weightedInputDatapoints = complexWeightApply(inputDatapoints, complexDimension, radiusArray, radiusAxis)
+    else:
+        weightedInputDatapoints = inputDatapoints
+    radialIntegrals = complexAngleIntegrator(weightedInputDatapoints, angleAxis)
+    return np.sum(radialIntegrals)
+
+
+def radialIntegrator(inputDatapoints, complexDimension, radiusArray=None, applyWeightBool=True):
+    if applyWeightBool:
+        weightedInputDatapoints = complexWeightApply(inputDatapoints, complexDimension, radiusArray, 0)
+    else:
+        weightedInputDatapoints = inputDatapoints
+    return np.sum(weightedInputDatapoints)
+
+
+def weightedDifference(largerData, smallerData, complexDimension, radiusArray=None, radiusAxis=0, normalizedBool=True):
+    if normalizedBool:
+        diffMatrix = complexWeightApply(np.abs(largerData-smallerData), complexDimension, radiusArray, radiusAxis)
+        integral = completeIntegrator(diffMatrix, complexDimension, radiusArray, radiusAxis=radiusAxis)
+        if integral > 0:
+            return diffMatrix/integral
+        else:
+            return diffMatrix
+    else:
+        return complexWeightApply(np.abs(largerData-smallerData), complexDimension, radiusArray, radiusAxis)
+
+
+def fourierCoefficientsNormalized(inputData, maxDeg, radiusAxis, angleAxis=1):
+    M = inputData.shape[angleAxis]
+    dTheta = 2 * np.pi / M
+    thetaArray = np.linspace(0, 2 * np.pi, M, endpoint=False)
+    A = inputData.shape[radiusAxis]
+    hat_a = np.zeros((A, maxDeg + 1))
+    hat_b = np.zeros((A, maxDeg + 1))
+
+    # k=0 term is just the mean:
+    hat_a[:, 0] = np.sum(inputData, axis=1) * dTheta
+
+    for k in range(1, maxDeg + 1):
+        cos_ktheta = np.cos(k * thetaArray)
+        sin_ktheta = np.sin(k * thetaArray)
+
+        # For each radius a, take dot product with cos/sin
+        hat_a[:, k] = np.tensordot(inputData, cos_ktheta, axes=([1], [0])) * dTheta
+        hat_b[:, k] = np.tensordot(inputData, sin_ktheta, axes=([1], [0])) * dTheta
+
+    return hat_a, hat_b
+
+def normalizeRadials(inputDatapoints, radiusAxis=0, angleAxis=1):
+    fr = complexAngleIntegrator(inputDatapoints, angleAxis)
+    fMask = np.logical_not(fr > 0)
+    fInt = radialIntegrator(fr, 0, applyWeightBool=False)
+    # fr /= fInt
+    fr[fMask] = 1
+    shape = [1] * inputDatapoints.ndim
+    shape[radiusAxis] = fr.shape[0]
+    frNDimArray = fr.reshape(shape)
+    normalizedDatapoints = inputDatapoints / frNDimArray
+    oNDimArray = fMask.reshape(shape)
+    normalizedDatapoints += oNDimArray
+    return normalizedDatapoints
+
+def complexDoubleCap(dim, outputArraySize,poleArraySize=0):
+    if poleArraySize == 0:
+        poleArraySize = outputArraySize
+    outputRadiusArray = np.linspace(0, 1, outputArraySize, endpoint=True,dtype=np.longdouble)
+    sqrdORA = np.square(outputRadiusArray)
+    remainderORA = np.sqrt(1-sqrdORA)
+    sqrdremainderORA = 1 - sqrdORA
+    outputArray = np.zeros(outputArraySize)
+    poleRadiusArray = np.linspace(math.sqrt(1/2), 1, poleArraySize, endpoint=True,dtype=np.longdouble)
+    sqrdPRA = np.square(poleRadiusArray)
+    remainderPRA = np.sqrt(1-sqrdPRA)
+    sqrdremainderPRA = 1 - sqrdPRA
+    poleSecondRadius = np.linspace(0, 1, poleArraySize, endpoint=True,dtype=np.longdouble)
+    sqrdSRA = np.square(poleSecondRadius)
+    # weights for the first coordinate where the cap is centered on
+    wpoleRadius = (((2**dim)*poleRadiusArray*(dim-1)*(np.power((1-np.square(poleRadiusArray)),(dim-2))))
+                   / poleArraySize * (1 - math.sqrt(1/2)))
+    # wpoleRadius /= np.sum(wpoleRadius)
+    # weights for the first coordinate where the cap is centered on
+    wspoleSecondRadius = (2*poleSecondRadius*(dim-2)*((1-np.square(poleSecondRadius))**(dim-3)))/poleArraySize
+    # wspoleSecondRadius /= np.sum(wspoleSecondRadius)
+    for oneIdx, (radOne, sqrdRadOne, remainderRadOne,sqrdRemainderRadOne,weightRadOne) in enumerate(
+            zip(poleRadiusArray,sqrdPRA,remainderPRA,sqrdremainderPRA,wpoleRadius)):
+        intermediateOutput = np.zeros(outputArraySize)
+        for twoIdx, (radTwo, sqrdRadTwo, weightRadTwo) in enumerate(zip(poleSecondRadius,sqrdSRA,wspoleSecondRadius)):
+            arccosNumeratorArray = sqrdORA*sqrdRadOne + sqrdremainderORA*sqrdRemainderRadOne*sqrdRadTwo - 1/2
+            arccosDenominatorArray = -2*outputRadiusArray*remainderORA*radOne*remainderRadOne*radTwo
+            ubArray = -arccosNumeratorArray-arccosDenominatorArray
+            lbArray = -arccosNumeratorArray+arccosDenominatorArray
+
+            maskMustBeInIt = (-arccosNumeratorArray-arccosDenominatorArray<=0)
+            maskCantBeInIt = (-arccosNumeratorArray + arccosDenominatorArray >= 0)
+            # if np.any(maskMustBeInIt[:-1]):
+            #     print(f"guaranteed {oneIdx},{twoIdx}")
+            # if np.any(maskCantBeInIt[0:]):
+            #     print(f"guaranteed not {oneIdx},{twoIdx}")
+            # arccosDenomMask = (arccosDenominatorArray == 0)
+            arccosFraction = np.clip(arccosNumeratorArray/arccosDenominatorArray,-1,1)
+            # accosFractionUnclipped = arccosNumeratorArray/arccosDenominatorArray
+            # if not np.all(arccosDenomMask):
+            #     print(np.max(accosFractionUnclipped[np.logical_not(arccosDenomMask)]))
+            #     print(np.min(accosFractionUnclipped[np.logical_not(arccosDenomMask)]))
+            arccosFraction[maskCantBeInIt] = 1
+            arccosFraction[maskMustBeInIt] = -1
+            arcCosArray = np.arccos(arccosFraction)/np.pi
+            # negarcCosArray = np.arccos(-arccosFraction)/np.pi
+
+            totalIterdy = weightRadTwo*arcCosArray
+            np.add(intermediateOutput, totalIterdy, out=intermediateOutput)
+        np.add(outputArray,weightRadOne*intermediateOutput,out=outputArray)
+
+    return outputArray
+
+
+if __name__ == '__main__':
+    cDim = 5
+    outputArraySize = 513
+    resolution = 1025
+    invariantDoubleComplexCap = complexDoubleCap(cDim,outputArraySize,resolution)
+    radialIntegral = radialIntegrator(invariantDoubleComplexCap, complexDimension=cDim)
+    DCC = np.zeros(outputArraySize)
+    rads = np.linspace(0,1,outputArraySize,endpoint=True)
+    radsMask = (rads>=math.sqrt(1/2))
+    DCC[radsMask] = 1
+    DCCIntegral = radialIntegrator(DCC,complexDimension=cDim)
+    randomSpherePoints = createRandomPointsReal(4, 7, checkForMistakesBool=True)
+    complexPointsRadii = complexSphereDensity(3,200)
+    complexPoints = np.ones((200,250))
+    shape = [1] * 2
+    shape[0] = 200
+    complexPointsRadii = complexPointsRadii.reshape(shape)
+    complexPoints = complexPoints * complexPointsRadii
+    normalizedDatapoints = normalizeRadials(complexPoints)
+    innerProductsMatrix = randomSpherePoints @ randomSpherePoints.T
+
+    #
+    # lp = LineProfiler()
+    # lp_wrapper = lp(complexTester)
+    # lp_wrapper()
+    # lp.print_stats()
+
+    print("done")
--- main.py
+++ main.py
@@ -4,15 +4,20 @@ import gurobipy as gp
 from gurobipy import GRB
 import math
 import scipy
+import numpy as np
 import fractions
 import random
+import SphereBasics
 from line_profiler import LineProfiler
 
 nrOfVars = 10
 nrOfTests = 200
-dimension = 12
+dimension = 4
 distanceForbidden = 0
 jacobiType = "new"
+
+polar2z = lambda r, theta: r * np.exp(1j * theta)
+z2polar = lambda z: (np.abs(z), np.angle(z))
 
 def thetaBasedOnThetapart(tpVal):
     return (-tpVal)/(1-tpVal)
@@ -58,6 +63,15 @@ def realDiskPoly(alpha, gamma, k, radius, angle):
     return (radius**gamma)*math.cos(gamma*angle*math.pi)*normedJacobiValue(k, alpha, gamma, float(2*(radius**2)-1))
 
 
+def vectorizeRDP(alpha, gamma, k, zArray):
+    jacobiLocs = 2*(np.abs(zArray)**2)-1
+    gammaFactor = np.real(np.power(zArray,gamma))
+    jacobiFactor = scipy.special.eval_jacobi(k, alpha, gamma, 1)
+    jacobiVals = scipy.special.eval_jacobi(k, alpha, gamma, jacobiLocs)
+    diskVals = gammaFactor*jacobiVals/jacobiFactor
+    return diskVals
+
+
 def negAngle(gamma, angle):
     totAngle = gamma*angle - 1/2
     totAngle = totAngle % 2.0
@@ -96,34 +110,6 @@ def makeModel(name):
     m.optimize()
     print(m.ObjVal)
 
-    # for slIdx in range(nrOfSls):
-    #     slindices, slvalues = getRow(aST, slIdx)
-    #     rowSize = len(slindices)
-    #     m.addConstr((gp.quicksum((tourWeight[slindices[clusterIdx]] * slvalues[clusterIdx])
-    #                              for clusterIdx in range(rowSize)) + absoluteErrors[slIdx]
-    #                  >= cl[slIdx]), name=f"positive errors Screenline {slIdx}")
-    #     m.addConstr((gp.quicksum((tourWeight[slindices[clusterIdx]] * slvalues[clusterIdx])
-    #                              for clusterIdx in range(rowSize)) - absoluteErrors[slIdx]
-    #                  <= cl[slIdx]), name=f"negative errors Screenline {slIdx}")
-    #
-    # m.addConstrs(
-    #     ((tourWeight[clusterIdx] - tourDeviation[clusterIdx] <= tbw[clusterIdx]) for clusterIdx in range(nrOfClusters)),
-    #     name="Positive deviation of cluster")
-    # m.addConstrs(
-    #     ((tourWeight[clusterIdx] + tourDeviation[clusterIdx] >= tbw[clusterIdx]) for clusterIdx in range(nrOfClusters)),
-    #     name="Negative deviation of cluster")
-    # # m.addConstrs(((tourWeight[ODpair] - 1 <= largeErrors[ODpair]) for ODpair in toursDF.index.tolist()),
-    # #              name="Large errors in tour")
-    # # m.addConstr(totalError == gp.quicksum(tourDeviation[idx]*tourDeviation[idx]*tComp[idx]/tbw[idx] for idx in range(nrOfClusters)), name=f"total error")
-    # m.addConstr(totalError == gp.quicksum(
-    #     tourDeviation[idx] * singleTComp for idx in range(nrOfClusters)),
-    #             name=f"total error")
-    #
-    # objective = (gp.quicksum(absoluteErrors[slIdx] for slIdx in range(nrOfSls))
-    #              + (
-    #                  totalError))  # + gp.quicksum(largeErrors[ODpair] for ODpair in toursDF.index.tolist())
-    # m.setObjective(objective, GRB.MINIMIZE)
-    # m.update()
 
 
 def makeModelv2(alpha, beta, newForbidden):
@@ -323,7 +309,99 @@ def tierListPrinter(Dictionary, nrPrinted=0):
               f", gamma {sortedDict2[placement][1][3]}, k {sortedDict2[placement][1][4]}.")
 
 
+def characteristicMatrix(nrOfPoints):
+    nrOfSets = 2**nrOfPoints
+    setIndices = np.arange(nrOfSets)
+    charMatrix = np.empty((nrOfPoints, nrOfSets), dtype=bool)
+    for pointIdx in range(nrOfPoints):
+        relativeIndices = setIndices//(2**pointIdx)
+        charIndices = relativeIndices % 2
+        charMatrix[pointIdx] = charIndices
+    return charMatrix
+
+
+# possible alterations to fix model:
+# is it a_0>=(sum_{0<=i<=inf}(a_i))^2 or a_0>=(sum_{1<=i<=inf}(a_i))^2
+# is it sum_{i in sets}(lambda_i)==1 or sum_{i in sets of v}(lambda_i)==1 for all v in V
+# is obj sum_{0<=i<=inf}(a_i) or sum_{1<=i<=inf}(a_i)
+# do we do Disk(v,v) = sum_{i in sets}(lambda_i I_{v in set i})
+
+def modelBQP(forbiddenRadius, forbiddenAngle, complexDimension, gammaMax, kMax, pointMatrix=None, nrOfPoints=2):
+    alpha = complexDimension - 2
+    forbiddenZ = polar2z(forbiddenRadius, forbiddenAngle)
+    if pointMatrix is None:
+        pointMatrix = SphereBasics.createRandomPointsComplex(complexDimension, nrOfPoints,
+                                                             baseZ=forbiddenZ)[0]
+    charMatrix = characteristicMatrix(nrOfPoints)
+    nrOfSets = charMatrix.shape[1]
+    nrOfPoints = pointMatrix.shape[0]
+    innerProductMatrix = pointMatrix @ pointMatrix.conj().T
+    diskPolyValueTensor = np.zeros((gammaMax, kMax, nrOfPoints, nrOfPoints))
+    for gamma in range(gammaMax):
+        for k in range(kMax):
+            diskPolyValueTensor[gamma][k] = vectorizeRDP(alpha, gamma, k, innerProductMatrix)
+    m = gp.Model(f"BQP model for forbidden innerproduct {forbiddenZ} in dimension {complexDimension}, "
+                 f"with {nrOfPoints} BQP points,"
+                 f" gamma checked {gammaMax}, k checked {kMax}")
+    m.setParam("OutputFlag", 1)
+    m.setParam(GRB.Param.DisplayInterval, 10)
+
+    diskPolyWeights = m.addVars(gammaMax, kMax, vtype=GRB.CONTINUOUS, lb=0, name="diskpoly weights")
+    setWeights = m.addVars(nrOfSets, vtype=GRB.CONTINUOUS, lb=0, name="set weights")
+
+    # m.addConstr(gp.quicksum(fList[fIter] * jacobiList[fIter] for fIter in range(nrOfVars)) <= 0,
+    #             name="Jacobi_ConstraintS")
+    # m.addConstr(gp.quicksum(fList[fIter] * jacobiList[fIter] for fIter in range(nrOfVars)) >= 0,
+    #             name="Jacobi_ConstraintL")
+    #
+    # m.addConstr((1 == gp.quicksum(fList[fIter]
+    #                               for fIter in range(nrOfVars))),
+    #             name=f"Measure Constraint")
+
+    # objective = (sphereMeasure**2 * fList[0])
+    # m.setObjective(fList[0], GRB.MAXIMIZE)
+    m.addConstr(setWeights.sum() == 1, name="convex description of sets")
+    m.addConstr(diskPolyWeights[0,0] - diskPolyWeights.sum()*diskPolyWeights.sum() >= 0, "SDP contraint")
+    m.addConstr(gp.quicksum(diskPolyValueTensor[gammaIdx,kIdx,1,0]*diskPolyWeights[gammaIdx,kIdx]
+                            for gammaIdx in range(gammaMax) for kIdx in range(kMax)) == 0,
+                "Forbidden Innerproduct Constraint")
+    constraintMatrx = [[0 for j in range(nrOfPoints)] for i in range(nrOfPoints)]
+    for mainPointIdx in range(nrOfPoints):
+        for secondPointIdx in range(mainPointIdx+1):
+            charVector = charMatrix[mainPointIdx]*charMatrix[secondPointIdx]
+            constraintMatrx[mainPointIdx][secondPointIdx] = m.addConstr(gp.quicksum(diskPolyValueTensor[gammaIdx, kIdx, mainPointIdx, secondPointIdx] *
+                                    diskPolyWeights[gammaIdx, kIdx]
+                                    for gammaIdx in range(gammaMax) for kIdx in range(kMax))
+                        ==
+                        gp.quicksum(setWeights[setIdx]*charVector[setIdx] for setIdx in range(nrOfSets)),
+                        f"BPQ  Constraint vectors {mainPointIdx}, {secondPointIdx}")
+    m.setObjective(diskPolyWeights.sum(), GRB.MAXIMIZE)
+    m.update()
+    m.optimize()
+    setSum = 0
+    setWeightsArray = np.zeros(nrOfSets)
+    for i in range(nrOfSets):
+        setSum += setWeights[i].x
+        setWeightsArray[i] = setWeights[i].x
+    diskSum = 0
+    diskWeightsArray = np.zeros((gammaMax, kMax))
+    for gammaIdx in range(gammaMax):
+        for kIdx in range(kMax):
+            diskSum += diskPolyWeights[gammaIdx,kIdx].x
+            diskWeightsArray[gammaIdx,kIdx] = diskPolyWeights[gammaIdx,kIdx].x
+    print(diskSum)
+    print(diskSum**2)
+    print((1/2)**(complexDimension-1))
+    print("Done")
+    return m, diskPolyWeights, setWeights
+
+
+
+
+
 if __name__ == '__main__':
+    testMatrix = characteristicMatrix(6)
+    modelBQP(0,0,3,15,15,nrOfPoints=14)
     makeModelv2((dimension-3.0)/2.0, (dimension-3.0)/2.0, 0)
     denomList = list(range(1,256))
     nonSimpleSolutions = {}
